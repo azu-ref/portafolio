13b07cdf3763022dd0eceebd41933dbea351b638
azu-ref <fjah17@gmail.com>
2019-07-09 14:49:05 -0400

    pagina responsive hasta 320px

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var email = document.getElementById('correo');
+const email = document.getElementById('correo');
 
     email.addEventListener("keyup", function (event) {
         if (email.validity.typeMismatch) {
@@ -7,3 +7,24 @@ var email = document.getElementById('correo');
             email.setCustomValidity("");
         }
     })
+
+const bgButton = document.getElementById('bg-button');
+const menu = document.querySelector('.menu');
+const link = document.querySelectorAll('.link');
+
+console.log(link);
+link.forEach(e => {
+    e.addEventListener('click', mostrarMenu);
+})
+
+bgButton.addEventListener('click', mostrarMenu);
+
+function mostrarMenu(){
+    if(menu.classList.contains('active')){
+        menu.classList.remove('active');
+
+    }else{
+        menu.classList.add('active');
+
+    }
+}
