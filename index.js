d63f2db2e5a3d0b00fe80aafb4377d184a5cc0ac
azu-ref <fjah17@gmail.com>
2019-07-17 22:52:11 -0400

    formulario activo

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const email = document.getElementById('correo');
+const name = document.getElementById('nombre');
+const message = document.getElementById('mensaje');
 
-    email.addEventListener("keyup", function (event) {
-        if (email.validity.typeMismatch) {
-            email.setCustomValidity("¡Necesito que ingreses una direccion de correo valida!");
-        } else {
-            email.setCustomValidity("");
-        }
-    })
+email.addEventListener("keyup", function (event) {
+    if (email.validity.typeMismatch) {
+        email.setCustomValidity("¡Necesito que ingreses una direccion de correo valida!");
+    } else {
+        email.setCustomValidity("");
+    }
+})
+
+const form = document.getElementById('contactform');
+
+form.addEventListener('submit', e => {
+    if(!email.value || !name.value || !message.value){
+        alert('Debe llenar todos los campos')
+        e.preventDefault();
+    }
+})
+
 
 const bgButton = document.getElementById('bg-button');
 const menu = document.querySelector('.menu');
